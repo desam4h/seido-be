d4b94b9317add21150aa7d0ac55d695f04cc6bcb
Jose Molina <desarrollo@m4h.com.co>
2017-12-22 14:02:45 -0500

    cambios cors

--- src/main/java/co/com/m4h/registros/config/WebSecurityConfig.java
+++ src/main/java/co/com/m4h/registros/config/WebSecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.google.common.collect.ImmutableList;
 
 import co.com.m4h.registros.security.JwtAuthenticationEntryPoint;
 import co.com.m4h.registros.security.JwtAuthenticationTokenFilter;
@@ -46,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.cors();
 		httpSecurity
 				// we don't need CSRF because our token is invulnerable
 				.csrf().disable()
@@ -67,5 +73,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// disable page caching
 		httpSecurity.headers().cacheControl();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(ImmutableList.of("*"));
+		configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+		// setAllowCredentials(true) is important, otherwise:
+		// The value of the 'Access-Control-Allow-Origin' header in the response
+		// must not be the wildcard '*' when the request's credentials mode is
+		// 'include'.
+		configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
