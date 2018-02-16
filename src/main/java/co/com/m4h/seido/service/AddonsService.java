8c77f3ab31c6c103aebbc9255b329e5606ea7327
Jose Molina <desarrollo@m4h.com.co>
2018-02-15 23:58:57 -0500

    Descarga excel control 6 meses OK

--- src/main/java/co/com/m4h/seido/service/AddonsService.java
+++ src/main/java/co/com/m4h/seido/service/AddonsService.java
@@ -1,5 +1,6 @@
 package co.com.m4h.seido.service;
 
+import java.io.File;
 import java.util.List;
 
 import co.com.m4h.seido.json.Control6Meses;
@@ -9,4 +10,6 @@ import co.com.m4h.seido.json.Control6Meses;
  */
 public interface AddonsService {
 	List<Control6Meses> findPatientsToControl();
+
+	File getExcel();
 }
--- src/main/java/co/com/m4h/seido/web/AddonsController.java
+++ src/main/java/co/com/m4h/seido/web/AddonsController.java
@@ -1,6 +1,12 @@
 package co.com.m4h.seido.web;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,5 +33,31 @@ public class AddonsController {
 	public ResponseEntity<List<Control6Meses>> findPatientsToControl() {
 		List<Control6Meses> controls = addonsService.findPatientsToControl();
 		return new ResponseEntity<>(controls, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/control6excel", method = RequestMethod.GET)
+	public void buildExcel(HttpServletResponse response) {
+
+		try {
+			File file = addonsService.getExcel();
+
+			response.addHeader("Content-Disposition", "attachment; filename=" + file.getName());
+			response.setContentType(Files.probeContentType(file.toPath()));
+			response.setContentLengthLong(file.length());
+
+			FileInputStream fis = new FileInputStream(file);
+			int c;
+			while ((c = fis.read()) > -1) {
+				response.getOutputStream().write(c);
+			}
+
+			response.flushBuffer();
+
+			fis.close();
+			file.delete();
+		} catch (IOException e) {
+			System.out.println("::::: error: " + e.getMessage());
+		}
+
 	}
 }
